b8764315c1601785202474f7c2de281e3a60697a
kr0nos <68698683+kr0nosKM@users.noreply.github.com>
2023-04-23 17:37:03 +0200

    Update usuariosScript.ps1

--- altres/usuariosScript.ps1
+++ altres/usuariosScript.ps1
@@ -20,5 +20,45 @@ ABEL,ZAFRILLA,PASCUAL,17629125,Alcoi,Gerencia,Jefe
 AITANA,VIDAL,RIBERA,68793380,Alcoi,Administración,Jefe
 ALEJANDRA,VIDAL,TORREGROSA,65006955,Alcoi,Administración,Miembro
 ================================
+PRECAUCIONES: Tener cuidado con los acentos, puede ser que explote algún comando por eso, probablmente eso habrá que solucionarlo.
 */
+# ========= VARIABLES ======== #
+
+  # [+] SEDE
+  $sede = "Barcelona"
+
+  # [+] DOMINIO
+  $dominio = W$sede".lan"
+  
+  # [+] UNIDAD ORGANIZATIVA RAÍZ
+  $raizUO = "BARCELONA"
+
+  # [+] LEER EL FICHERO .CSV
+  $ruta = "......ruta......\usuaris.csv"
+  $ficheroCSV = import-csv $ruta
+  
+  # [+] LISTA ACTUAL DE LAS UNIDADES ORGANIZATIVAS
+  $listaActualUO = Get-ADOrganizationalUnit -Filter 'Name -like "*"'
+
+  # [+] DEPARTAMENTOS
+  # Necesitaremos meter el nombre de los departamentos en un array para la creación de las UOs y Grupos
+  $departamentos = $(
+    foreach ($lineaInfo in $ficheroCSV){
+      $lineaInfo.dept
+    }
+  ) | sort-object | get-unique
+
+  
+# ========= CÓDIGO =========== #
+
+# [+] COMPROBACIÓN SEDE
+foreach ($sede in $ficheroCSV){
+  if ($ficheroCSV.sede -match $sede){
+    
+    ...................CÓDIGO...............
+    
+  }
+}
+
+
 
