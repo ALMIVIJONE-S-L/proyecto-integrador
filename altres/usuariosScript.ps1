5ed0fc7b963a8b2a9faf945188e1d60e35f97614
kr0nos <68698683+kr0nosKM@users.noreply.github.com>
2023-04-23 16:46:04 +0200

    Create usuariosScript.ps1

---
+++ altres/usuariosScript.ps1
@@ -0,0 +1,24 @@
+/*
+SCRIPT PARA LA CREACIÓN DE USUARIOS
+Recordar: Este script al igual que el csv, deben de estar en las respectivas carpetas donde se hacen los backups y tal
+
+Carácterísticas del script:
+- CREAR UOs
+- CREAR GRUPOS, AÑADIRLOS A UOs
+- CREAR USUARIOS, AÑADIRLOS A GRUPOS Y UOs
+- [SUGERENCIA] PERFILES MÓVILES AUTOMÁTICAMENTE
+- [SUGERENCIA] PERFILES OBLIGATORIOS AUTOMÁTICAMENTE
+- [SUGERENCIA] CARPETA PERSONAL AUTOMÁTICAMENTE
+*/
+
+/*
+================================
+EJEMPLO DEL .CSV DE LOS USUARIOS:
+================================
+nom,cognom1,cognom2,dni,sede,dept,descrip
+ABEL,ZAFRILLA,PASCUAL,17629125,Alcoi,Gerencia,Jefe
+AITANA,VIDAL,RIBERA,68793380,Alcoi,Administración,Jefe
+ALEJANDRA,VIDAL,TORREGROSA,65006955,Alcoi,Administración,Miembro
+================================
+*/
+
