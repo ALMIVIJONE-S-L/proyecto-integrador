7fac720f727a5a730011ed0b25652f5b26ab9cc7
kr0nos <68698683+kr0nosKM@users.noreply.github.com>
2023-06-02 23:43:21 +0200

    Update login.js

--- altres/intranet/js/login.js
+++ altres/intranet/js/login.js
@@ -1,1 +1,120 @@
+/*JAVASCRIPT DEL LOGIN*/
+let nomUsrs = ["miguel","jose ivan","victor","alberto"];
+let pwdUsrs = ["1234","1234","1234","1234"]; // Supongamos que son diferentes por cada persona
 
+// Comprobar si el usuario existe en el array
+let contadorUsr = 0;
+let contadorUsrCorrecto = 0;
+function usrExiste(nombre){
+    // Extraido de "https://www.30secondsofcode.org/js/s/remove-accents/" para poder quitar los acentos usando el código UNICODE
+    const removeAccents = str => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    nombre = removeAccents(nombre.toLowerCase());
+
+    let divUsuario = document.getElementById("divUsuario");
+
+    for (let i = 0; i < nomUsrs.length; i++){
+        let estadoActual = "";
+        let estadoOld = "";
+        let texto = "";
+
+        if (nomUsrs[i] == nombre){
+            estadoActual = "valid";
+            texto = "Correcto."
+        }
+        else{
+            estadoActual = "invalid";
+            texto = "Nombre incorrecto, por favor, elija otro."
+
+        }
+
+        if(estadoActual != estadoOld && contadorUsr == 0){
+            // divu....getElementId..(feedbackBlockUser).class cambiarle la class y cambiarle el texto (con inner)
+            // Que se cree el div una única vez y luego ya es modificarlo, creo que será lo mejor.
+            divUsuario.innerHTML = (
+            divUsuario.innerHTML +   ` 
+                                    <div id="feedbackUser" class="`+ estadoActual + `-feedback d-block ms-1">
+                                        ` + texto + `
+                                    </div>`)
+            estadoOld = estadoActual;
+            contadorUsr++
+
+            if(estadoActual == "valid"){
+                document.getElementById("usuario").value = nombre
+                return true;
+            }
+        }
+        else if(estadoActual != estadoOld){
+            let divFeedback = document.getElementById("feedbackUser");
+            divFeedback.className = estadoActual + "-feedback d-block ms-1";
+            divFeedback.innerHTML = texto;
+
+            if(estadoActual == "valid"){
+                document.getElementById("usuario").value = nombre
+                return true;
+            }
+
+        }
+        else{
+            continue;
+        }
+    }
+}
+
+let contadorPwd = 0;
+function pwdExiste(password){    
+    let divPassword = document.getElementById("divPassword");
+    for (let i = 0; i < pwdUsrs.length; i++){
+
+            let estadoActual = "";
+            let estadoOld = "";
+            let texto = "";
+    
+            if (pwdUsrs[i] == password){
+                return true;
+            }
+            else{
+                estadoActual = "invalid";
+                texto = "Contraseña incorrecta, por favor, introduzca una válida"
+            }
+    
+            if(estadoActual != estadoOld && contadorPwd == 0){
+                // divu....getElementId..(feedbackBlockUser).class cambiarle la class y cambiarle el texto (con inner)
+                // Que se cree el div una única vez y luego ya es modificarlo, creo que será lo mejor.
+
+                divPassword.innerHTML = (
+                divPassword.innerHTML +   ` 
+                                        <div id="feedbackPwd" class="`+ estadoActual + `-feedback d-block ms-1">
+                                            ` + texto + `
+                                        </div>`)
+                estadoOld = estadoActual;
+                contadorPwd++
+            }
+
+            else if(estadoActual != estadoOld){
+                let divFeedback = document.getElementById("feedbackPwd");
+                divFeedback.className = estadoActual + "-feedback d-block ms-1";
+                divFeedback.innerHTML = texto;
+            }
+            else{
+                continue;
+            }
+
+            
+        
+    }
+}
+
+function procesarLogin(){
+    let nombreUsuario = document.getElementById("usuario").value;
+    let passwordUsuario = document.getElementById("password").value;
+
+    let usrBool = usrExiste(nombreUsuario)
+    let pwdBool = pwdExiste(passwordUsuario)
+
+    console.log(usrBool + " = " + pwdBool)
+    if (usrBool && pwdBool){
+        // Para cambiar de directorio
+        location.href = "index.html"
+    }
+
+}
